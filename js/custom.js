1c3030dde8e73f500001e5f429b13f2901885208
schaudhry123 <schaudhry123@gmail.com>
2015-01-17 21:23:28 -0800

    Image refit

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,47 @@
-// $("a[data-role=tab]").each(function () {
-//     var anchor = $(this);
-//     anchor.bind("click", function () {
-//         $.mobile.changePage(anchor.attr("href"), {
-//             transition: "none",
-//             changeHash: false
-//         });
-//         return false;
-//     });
-// });
+$(window).load(function(){
+    $('#content').imagefit();
+});
 
-// $("div[data-role=page]").bind("pagebeforeshow", function (e, data) {
-//     $.mobile.silentScroll(0);
-//     $.mobile.changePage.defaults.transition = 'slide';
-// });
 
-// $(function() {
-//     $("#tabs").tabs();
-// })
 
- $(function() {
-    $(document).delegate('[data-role="navbar"] a', 'click', function () {
-        $('[data-role="navbar"] a').removeClass("ui-btn-active");
-            $('.tabs-content').children().hide();
-        $('#' + $(this).attr('data-tab')).show();
-            $("html,body").animate({scrollTop:0},500);
-            $(this).addClass("ui-btn-active");
-    });
+
+ // $(function() {
+ //    $(document).delegate('[data-role="navbar"] a', 'click', function () {
+ //        $('[data-role="navbar"] a').removeClass("ui-btn-active");
+ //            $('.tabs-content').children().hide();
+ //        $('#' + $(this).attr('data-tab')).show();
+ //            $("html,body").animate({scrollTop:0},500);
+ //            $(this).addClass("ui-btn-active");
+ //    });
+
+
+
+// window.onresize = window.onload = function()
+// {
+//     resize();
+// }
+
+// function resize()
+// {
+//     var img    = document.getElementsByTagName('img')[0];
+//         winDim = getWinDim();
+
+
+//     img.style.height = winDim.y + "px";
+
+//     if (img.offsetWidth > winDim.x)
+//     {
+//         img.style.height = null;
+//         img.style.width = winDim.x + "px";
+//     }
+// }
+
+// function getWinDim()
+// {
+//     var body = document.documentElement || document.body;
+
+//     return {
+//         x: window.innerWidth  || body.clientWidth,
+//         y: window.innerHeight || body.clientHeight
+//     }
+// }
