63697b8e5798bc663b48e87bab276db570d83bfc
schaudhry123 <schaudhry123@gmail.com>
2015-01-18 16:21:10 -0800

    Pinch zoom test

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,3 @@
-$(document).bind('mobileinit', function() {
-    $.mobile.metaViewportContent = 'width=device-width';
+$(document).bind( "mobileinit", function(event) {
+       $.extend($.mobile.zoom, {locked:false,enabled:true});
 });
-
-// $(document).bind( "mobileinit", function(event) {
-//        $.extend($.mobile.zoom, {locked:false,enabled:true});
-// });
